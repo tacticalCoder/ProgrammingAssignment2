e8ccc0a3794c6f57cafc32d6f6bacf59b860a2ef
tacticalCoder <kjones.roguesoftware@gmail.com>
2022-02-07 08:48:39 -0800

    added comment

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(m = matrix()) {
     ## Init
     i <- NULL
